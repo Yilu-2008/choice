f27f997764083f8aaa1f2ac1f5d1012dc0997847
璐璐 <lulu@MacBook-Air.local>
2021-05-16 23:02:12 +0800

    new new

---
+++ choice.py
@@ -0,0 +1,22 @@
+rain = input('今天喝咖啡了吗')
+if rain == '有':
+   print('好开心','再来一杯')
+else:
+	print('为神马不买')
+
+
+age = input ('请输入年龄：')
+age = int(age)
+if age < 13:
+	print('儿童')
+elif age >= 13 and age <= 18:
+    print ('青少年')	
+elif age >= 19 and age <= 22:
+	print ('青年')
+else:
+	print('老年')
+
+
+
+
+
